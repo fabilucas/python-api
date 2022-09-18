0ee69aa6f48f833c9402ed6585dab778cc2de1cc
fabilucas <flucas.fabiane@gmail.com>
2022-09-18 00:16:18 -0300

    busca de municipios por cd da uf

--- src/main.py
+++ src/main.py
@@ -5,24 +5,24 @@ def main():
 
     log = logger.Logger("main")
 
-    log.info("---------LOCALIDADE IBGE--------")
+    log.info("---------LISTA MUNICIPIOS POR UF--------")
 
-    cep_input = input('Digite o CEP para a consulta: ')
+    cd_uf = input('Digite o código da Unidade da Federação: ')
 
-    if len(cep_input) != 8:
+    if len(cd_uf) != 2:
         log.error('Quantidade de dígitos inválida!')
         exit()
 
-    request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep_input))
+    request = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/{}/municipios'.format(cd_uf))
 
-    address_data = request.json()
+    municipios_por_uf = request.json()
 
-    if 'erro' not in address_data:
+    if 'erro' not in municipios_por_uf:
         log.info("Request success!")
-        log.info(address_data)
+        log.info(municipios_por_uf)
         
     else:
-        log.error('{}: CEP inválido.'.format(cep_input))
+        log.error('{}: Codigo da UF inválido.'.format(cd_uf))
 
     option = int(input('Deseja realizar uma nova consulta ?\n1. Sim\n2. Sair\n'))
     if option == 1:
